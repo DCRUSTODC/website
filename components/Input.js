1d973714f3da7a157da4529da740e03933483a79
thatsameguyokay <90185950+thatsameguyokay@users.noreply.github.com>
2022-04-09 14:53:35 +0530

    Fixed Search input
    Entry was restricted due to useEffect hook

--- components/Input.js
+++ components/Input.js
@@ -2,14 +2,8 @@ import Image from 'next/image';
 import React, { useState, useEffect } from 'react';
 
 const Input = () => {
-  const [isFocused, setIsFocused] = useState(false);
-
-  const find = () => {
-    setIsFocused(true);
-  }
 
   return (
-    isFocused ? (
       <div className='relative'>
         <div className='absolute top-[10px] left-3'>
           <Image src='/icons/search.svg' alt='search' width={20} height={20} />
@@ -21,20 +15,7 @@ const Input = () => {
           onChange={find}
         />
       </div>
-    ) : (
-      <div className='relative'>
-        <div className='absolute top-[10px] left-3'>
-          <Image src='/icons/search.svg' alt='search' width={20} height={20} />
-        </div>
-        <input
-          type={'text'}
-          placeholder='Search for ongoing projects'
-          className='bg-[#ffffff1a] outline-none py-2 pl-10 pr-5 rounded-xl'
-          onChange={find}
-        />
-      </div>
-    )
-  );
+    );
 };
 
 export default Input;
