7a818a5a71da0817f1143e0612b39582de6354fd
Harsh Prajapati <harsh@Harshs-MacBook-Air-2.local>
2023-05-23 00:09:00 +0530

    updated leaderboard & events(+ mobile version)

--- components/Leaderboard.js
+++ components/Leaderboard.js
@@ -1,77 +1,67 @@
 import { top_coders } from "@/assets/data/top-coders";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Container } from ".";
 import { Reveal } from "react-awesome-reveal";
 import { fadeInDownShorter } from "@/keyframes";
 import { Button } from ".";
 
 const Leaderboard = () => {
-  return (
-    <Container className={"mb-44"}>
-      <center>
-        <h2 className="text-2xl font-bold text-center mb-5">
-          Top ranks in Weekly Contests
-        </h2>
-      </center>
-      <hr />
-      <div className="text-center mb-8 my-4">
-        {top_coders
-          .filter((curr) => curr.rank <= 3 || curr.id <= 3)
-          .map((item, i) => (
-            <Reveal
-              key={item.id}
-              keyframes={fadeInDownShorter}
-              duration={500}
-              delay={100 * (i + 1)}
-            >
-              <div
-                style={{
-                  display: "grid",
-                  gridTemplateColumns: "auto auto auto",
-                  marginBottom: item.id === 0 ? "0.5rem" : null,
-                }}
-              >
-                <div
-                  style={{
-                    maxWidth: "25rem",
-                    minWidth: "25rem",
-                  }}
-                >
-                  {item.id === 0 ? (
-                    item.name
-                  ) : (
-                    <a href={item.url} target={"_blank"}>
-                      {item.name}
-                    </a>
-                  )}
-                </div>
-                <div
-                  style={{
-                    maxWidth: "25rem",
-                    minWidth: "25rem",
-                  }}
-                >
-                  {item.rank}
-                </div>
-                <div
-                  style={{
-                    maxWidth: "25rem",
-                    minWidth: "25rem",
-                  }}
-                >
-                  {item.score}
-                </div>
-              </div>
-            </Reveal>
-          ))}
-      </div>
-      <div className="text-center">
-        <Button isLink href={"/standings"} variant={"outline-primary"}>
-          Final standings
-        </Button>
-      </div>
-    </Container>
-  );
+    return (
+        <Container className={"mb-44"}>
+            <center>
+                <h2 className="text-2xl font-bold text-center mb-5">
+                    Top ranks in Weekly Contests
+                </h2>
+            </center>
+            <hr />
+            <div className="text-center mb-8 my-4">
+                {top_coders
+                    .filter((curr) => curr.rank <= 3 || curr.id <= 3)
+                    .map((item, i) => (
+                        <Reveal
+                            key={item.id}
+                            keyframes={fadeInDownShorter}
+                            duration={500}
+                            delay={100 * (i + 1)}
+                        >
+                            <div
+                                style={{
+                                    display: "grid",
+                                    gridTemplateColumns:
+                                        "repeat(2, minmax(0,1fr))",
+                                    marginBottom:
+                                        item.id === 0 ? "0.5rem" : null,
+                                }}
+                            >
+                                <div
+                                    style={{
+                                        padding: "0.2rem",
+                                        gridColumn: "span 1",
+                                    }}
+                                >
+                                    <a href={item.url} target={"_blank"}>
+                                        {item.name}
+                                    </a>
+                                </div>
+                                <div
+                                    style={{
+                                        padding: "0.2rem",
+                                        gridColumn: "span 1",
+                                    }}
+                                >
+                                    {item.rank}
+                                </div>
+                            </div>
+                        </Reveal>
+                    ))}
+            </div>
+            <div className="text-center">
+                <Button isLink href={"/standings"} variant={"outline-primary"}>
+                    Final standings
+                </Button>
+            </div>
+        </Container>
+    );
 };
 
 export default Leaderboard;
