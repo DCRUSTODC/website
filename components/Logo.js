1cb4958cc4c990e83a1498e602e6fe7349fafefb
Sambhav Saxena <sambhavsaxena02@gmail.com>
2023-10-08 13:36:12 +0530

    edit: replaces para tag with span seo reads paragraph tags separately. Also the reason why google says `dcrust odc` instead of `dcrustodc`

--- components/Logo.js
+++ components/Logo.js
@@ -1,17 +1,14 @@
 import React from "react";
 
 const Logo = () => {
-    return (
-        <a href="/">
-            <div
-                className="font-bold flex text-2xl"
-                style={{ cursor: "pointer" }}
-            >
-                <p>DCRUST</p>
-                <p className="text-[#000]">ODC</p>
-            </div>
-        </a>
-    );
+  return (
+    <a href="/">
+      <div className="font-bold flex text-2xl" style={{ cursor: "pointer" }}>
+        <p>DCRUST</p>
+        <span className="text-[#000]">ODC</span>
+      </div>
+    </a>
+  );
 };
 
 export default Logo;
